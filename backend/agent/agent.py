be2ac2ec19792bc0fdb80c19b1a57d7bf219f0f6
Charles Graydon <cgraydon1@sheffield.ac.uk>
2026-06-18 20:46:42 +0100

    Adding agent response

--- backend/agent/agent.py
+++ backend/agent/agent.py
@@ -1,10 +1,12 @@
 import json
+from unittest import result
 from urllib import response
 
 from agent.tool_registry import TOOLS, TOOL_DESCRIPTIONS
 from llm.claude import ClaudeClient
 
 llm = ClaudeClient()
+
 def choose_tool(user_query: str) -> dict:
     prompt = f"""
 {TOOL_DESCRIPTIONS}
@@ -33,13 +35,29 @@ def execute_tool(tool_call: dict):
 
     return tool_function(**args)
 
+def respond(user_query: str, tool_output: dict) -> str:
+    prompt = f"""
+
+
+User query:
+{user_query}
+
+Tool output:
+{json.dumps(tool_output, indent=2, default=str)}
+Respond to the users query with the information provided.
+"""
+
+    response = llm.generate(prompt)
+
+    return response.strip()
+
 
 def run_agent(user_query: str):
     tool_call = choose_tool(user_query)
 
-    result = execute_tool(tool_call)
+    tool_resposnse = execute_tool(tool_call)
 
-    return {
-        "tool_used": tool_call["tool"],
-        "result": result,
-    }
+    response = respond(user_query, tool_resposnse)
+
+    return response
+    
